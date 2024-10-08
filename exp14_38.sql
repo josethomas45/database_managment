2c7470f60f30fe7590334aa176fe1997f4921023
Jose Thomas <145546568+josethomas45@users.noreply.github.com>
2024-10-08 12:38:49 +0530

    Update exp14_38.sql

--- exp14_38.sql
+++ exp14_38.sql
@@ -1,4 +1,4 @@
- set serveroutput on;
+set serveroutput on;
 show serveroutput;
 create or replace function factorial(n int)
 return int as fact INT;
@@ -19,4 +19,22 @@ c:=&c;
 result:=factorial(c);
 dbms_output.put_line('The factorial is '||result);
 end;
+/
+
+CREATE OR REPLACE FUNCTION totalCustomers
+RETURN number IS
+   total number(2) := 0;
+BEGIN
+   SELECT count(*) into total
+   FROM CUSTOMER_38;
+   RETURN total;
+END;
+/
+
+DECLARE 
+   c number(2); 
+BEGIN 
+   c := totalCustomers(); 
+   dbms_output.put_line('Total no. of Customers: ' || c); 
+END; 
 /
